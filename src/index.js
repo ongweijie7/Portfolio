1bc91fbf255c841ba57ada18a0ea79143c147ba2
Wei Jie <ongweijie7@gmail.com>
2023-07-17 00:24:32 +0800

    Add media queries for dynamic resizing of widths 450, 800 and 1200

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter,BrowserRouter, Route, Routes } from "react-router-dom";
 
 import App from './App';
 import Transition from './sections/Transition';
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const renderApp = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path="/" element={<Transition/>}/>
       <Route path="/home" element={<App/>}/>
